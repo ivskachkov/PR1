04e69a30307ca541af00406f4ae62de0f1073ae5
Ivan Skachkov <vanaskackov589@gmail.com>
2023-09-09 18:07:26 +0300

    2nd push

--- main.c
+++ main.c
@@ -1,37 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "roman_converter.h"
+#include "defines.h"
 
-void convert(int value, char *out){
-    char c1 = 'X';
-    char c5 = 'L';
-    char c10 = 'C';
-    if(value == 4){
-        *out++ = c1;
-        *out++ = c5;
-    }else if(value == 9){
-        *out++ = c1;
-        *out++ = c10;
-    }else{
-        if(value > 4){
-            *out++ = c5;
-            value = value - 5;
-        }
-        for(int i = 0; i < value; i++){
-            *out++ = c1;
-        }
-    }
-}
+
 
 int main(int argc, char **argv){
-    char str[5] = {'\0'};
+    char str[16] = {'\0'};
+    int result = 0;
     int value = atoi(argv[1]);
-    convert(value, str);
-    printf("%s \n", str);
-
-    int a = 7;
-    int *p = &a;
-    *p = 78;
-    printf("%d \n" , a);
-    return 0;
+    result = convertA2R(value, str);
+    if(result != STATUS_OK){
+        ERR("error - value out of range. Enter value from range from 1 to %d\n", ROMAN_MAX);
+        return result;
+    }
+    LOG("%s \n", str);
+    return STATUS_OK;
 }
